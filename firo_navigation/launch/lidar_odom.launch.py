85b1b39d2af6db89b1872060fbd80e0dbeb3d5de
YoshidoSan <hurin65@gmail.com>
2025-07-29 13:40:06 +0200

    feat: add slam mapping and lidar range-flow odometry

---
+++ firo_navigation/launch/lidar_odom.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+            Node(
+                package='rf2o_laser_odometry',
+                executable='rf2o_laser_odometry_node',
+                name='rf2o_laser_odometry',
+                output='screen',
+                parameters=[{
+                    'laser_scan_topic' : '/ldlidar_node/scan',
+                    'odom_topic' : '/lidar_rf_odom',
+                    'publish_tf' : False,
+                    'base_frame_id' : 'base_link',
+                    'odom_frame_id' : 'odom',
+                    'init_pose_from_topic' : '',
+                    'freq' : 5.0}],
+            ),
+    ])
---
+++ firo_navigation/launch/slam_lidar.launch.py
@@ -0,0 +1,128 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, LifecycleNode
+
+
+def generate_launch_description():
+    
+    node_name = LaunchConfiguration('node_name')
+
+    # lifecycle manager configuration file for ldlidar node
+    lc_mgr_config_path = os.path.join(
+        get_package_share_directory('ldlidar_node'),
+        'params',
+        'lifecycle_mgr_slam.yaml'
+    )
+
+    # slam toolbox configuration
+    slam_config_path = os.path.join(
+        get_package_share_directory('firo_navigation'),
+        'config',
+        'slam_params.yaml'
+    )
+
+    # Lifecycle manager node
+    lc_mgr_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager',
+        output='screen',
+        parameters=[
+           # YAML files
+           lc_mgr_config_path  # Parameters
+        ]
+    )
+
+    # slam toolbox node in async mode
+    slam_toolbox_node = LifecycleNode(
+          package='slam_toolbox',
+          executable='async_slam_toolbox_node',
+          namespace='',
+          name='slam_toolbox',
+          output='screen',
+          parameters=[
+            # YAML files
+            slam_config_path # Parameters
+          ]        
+    )
+
+    # include ldlidar node launch
+    ldlidar_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource([
+            get_package_share_directory('firo_navigation'),
+            '/launch/lidar_bringup.launch.py'
+        ]),
+        launch_arguments={
+            'node_name': 'ldlidar_node'
+        }.items(), 
+    )
+
+    # fake odom publisher
+    fake_odom = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='fake_odom_to_base_link',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
+    )
+    # lidar base transform
+    base_to_lidar = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_to_lidar',
+        output='screen',
+        arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'ldlidar_base']
+    )
+    # fake footprint transform
+    fake_base_to_footprint = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='fake_base_to_footprint',
+        output='screen',
+        arguments=['0', '0', '-0.1', '0', '0', '0', 'base_link', 'base_footprint']
+    )
+
+    # RVIZ2 settings
+    rviz2_config = os.path.join(
+        get_package_share_directory('ldlidar_node'),
+        'config',
+        'ldlidar_slam.rviz'
+    )
+
+    # RVIZ2node
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=[["-d"], [rviz2_config]]
+    )
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
+
+    # Launch Nav2 Lifecycle Manager
+    ld.add_action(lc_mgr_node)
+
+    # Launch SLAM Toolbox node
+    ld.add_action(slam_toolbox_node)
+
+    # Launch fake odom and footprint publisher node
+    ld.add_action(fake_odom)
+    ld.add_action(fake_base_to_footprint)
+
+    # Launch static transfrom publisher nodes
+    ld.add_action(base_to_lidar)
+
+    # Call LDLidar launch
+    ld.add_action(ldlidar_launch)
+
+    # Start RVIZ2
+    ld.add_action(rviz2_node)
+
+    return ld
