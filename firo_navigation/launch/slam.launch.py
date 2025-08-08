b615d61097a332ea313194095511edeb657de000
YoshidoSan <hurin65@gmail.com>
2025-08-08 11:17:22 +0000

    wip:code to be tested on firo

--- firo_navigation/launch/slam.launch.py
+++ firo_navigation/launch/slam.launch.py
@@ -66,7 +66,7 @@ def generate_launch_description():
     ld.add_action(slam_toolbox_node)
 
     # Launch fake odom publisher node
-    ld.add_action(fake_odom)
+    #ld.add_action(fake_odom)
 
     # Launch fake footprint publisher node
     ld.add_action(base_to_footprint)
